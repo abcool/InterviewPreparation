4f641371ec69a20f3ba0fa1a4129501410444fea
Arvind Bakshi <arvindbakshicool@gmail.com>
2024-12-15 19:01:42 +0530

    Added Check kth bit is set problem

--- DSA/Bitwise/SetBit.java
+++ DSA/Bitwise/SetBit.java
@@ -1,7 +1,29 @@
-package Bitwise;
+import java.util.Scanner;
 
 public class SetBit {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = scan.nextInt();
+        System.out.println("Enter the bit place to be checked: ");
+        int k = scan.nextInt();
+        System.out.println(" Is the kth bit set: "+ isKthBitSet(n, k));
+        System.out.println(" Is the kth bit set[Right shift approach]: "+ isKthBitSetUsingRightShift(n, k));
+    }
+
+    private static boolean isKthBitSet(int n, int k){
+        int newNum = (1<<(k-1)); // get the new number 2^(k-1)
+        if((newNum&n)!=0)
+            return true;
+        else
+            return false;
+    }
+
+    private static boolean isKthBitSetUsingRightShift(int n, int k){
+        int newNum = (n>>(k-1)); // n/2^k-1
+        if((newNum & 1)!=0)   // check rightmost bit is 1
+            return true;
+        else
+            return false;
     }
 }
